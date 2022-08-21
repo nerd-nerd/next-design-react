58c81384cbd9bac61cbb4cfadec75943e3371448
yanquanfahei <1449826851@qq.com>
2022-08-21 18:44:10 +0800

    feat: remove foo

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,5 @@
  * @LastEditTime: 2022-08-21 15:32:11
  * @Description: 请填写简介
  */
-export { default as Foo } from './Foo';
-export { default as Button } from './Button';
-export { default as Input } from './input';
+export { default as Button } from './button'
+export { default as Input } from './input'
