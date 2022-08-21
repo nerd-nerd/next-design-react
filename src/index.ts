be2a97c8729b2e2d92039b3e0c1771b91086bff1
nemo <nemo@nemodeMacBook-Pro.local>
2022-08-21 18:59:49 +0800

    feat: build

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@
  * @Author: xiaoqing
  * @Date: 2022-08-21 13:25:57
  * @LastEditors: xiaoqing
- * @LastEditTime: 2022-08-21 15:32:11
+ * @LastEditTime: 2022-08-21 18:45:51
  * @Description: 请填写简介
  */
-export { default as Button } from './button'
-export { default as Input } from './input'
+export { default as Button } from './button';
+export { default as Input } from './input';
